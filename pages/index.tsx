afdd53390af7748245112385d48bf96a74bb93b4
kodai3 <k3dai.su3@gmail.com>
2020-10-07 08:30:47 +0900

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,47 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { createWorker } from 'tesseract.js';
+import * as React from "react"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const [file, setFile] = React.useState<any>();
+  const [textOcr, setTextOcr] = React.useState('');
+  const worker = createWorker({
+    logger: m => console.log(m)
+  })
+
+  const tryOcr = async() => {
+    await worker.load();
+    await worker.loadLanguage('jpn');
+    await worker.initialize('jpn');
+    const { data: { text } } = await worker.recognize(file);
+    setTextOcr(text);
+    await worker.terminate();
+  }
+
+  // fileData 取得
+  const handleChange = (e: any) => {
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0])
+  }
+
+  const handleClick = async() => {
+    if (!file) return
+    setTextOcr('Recognizing...')
+    await tryOcr();
+  }
+  
+  return (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+    <input type="file" onChange={handleChange} /><br />
+    <button className="button" onClick={handleClick}>Try OCR</button>
     <p>
       <Link href="/about">
-        <a>About</a>
+        <a>{textOcr}</a>
       </Link>
     </p>
   </Layout>
-)
+)}
 
 export default IndexPage
